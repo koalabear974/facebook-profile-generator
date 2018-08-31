cdb73e23a2219f6339edfc286d5d30327638f298
Unknown <adrien.robert@skoove.com>
2018-08-31 18:02:18 +0200

    Add overlay management and calculation update style and add helper

--- src/components/ImageManager.jsx
+++ src/components/ImageManager.jsx
@@ -1,33 +1,139 @@
 import React, {Component} from 'react';
+import $ from 'jquery';
 import PropTypes from 'prop-types';
+import ImageOnLoad from 'react-image-onload';
 import Button from '@material-ui/core/Button';
+import imageManagerHelper from './../helpers/imageManagerHelper.jsx';
 
 class ImageManager extends Component {
     constructor(props) {
         super(props);
 
+        let defaultValues = imageManagerHelper.defaultValues();
+
         this.state = {
+            imageLoaded: false,
+            calculationRun: false,
+            invalid: false,
+            imageProportions: defaultValues.imageProportions,
+            overlayProportions: defaultValues.overlayProportions,
         };
 
+        this.proportions = defaultValues.proportions;
+
+        this.bigOverlay = React.createRef();
+        this.smallOverlay = React.createRef();
+        this.image = React.createRef();
+
         this.onResetClick = this.onResetClick.bind(this);
+        this.componentDidUpdate = this.componentDidUpdate.bind(this);
+        this.onApplyClick = this.onApplyClick.bind(this);
+        this.onImageLoad = this.onImageLoad.bind(this);
+        this.updateCalculation = this.updateCalculation.bind(this);
     };
 
-    onResetClick(event) {
-        console.log(event);
+    onResetClick(e) {
         this.props.onResetClick();
     }
 
-    componentDidUpdate( prevProps,  prevState) {
+    onImageLoad(img) {
+        if (img.naturalHeight > img.naturalWidth) {
+            img.style = "height: 100%; width: auto;";
+        }
+
+        this.setState({
+            imageLoaded: true,
+            calculationRun: false,
+        });
+
+        setTimeout(() => {
+            this.updateCalculation();
+        }, 500);
+    }
+
+    onApplyClick(e) {
+    }
+
+    componentDidUpdate(prevProps) {
+    }
+
+    updateCalculation() {
+        let img = this.image.current.image;
+
+        let imageProportions = {
+            "width": img.width,
+            "height": img.height,
+            "naturalWidth": img.naturalWidth,
+            "naturalHeight": img.naturalHeight,
+        };
+
+        if (imageProportions.naturalWidth < this.proportions.small.bigPicture.width) {
+            this.setState({
+                invalid: true,
+                calculationRun: false,
+                imageProportions: imageProportions,
+            });
+        } else {
+            let overlayProportions = imageManagerHelper.calculateProportions(
+                imageProportions,
+                this.state.overlayProportions,
+                this.proportions
+            );
+
+            this.bigOverlay.current.style.width = overlayProportions.visible.bigPicture.width + "px";
+            this.bigOverlay.current.style.height = overlayProportions.visible.bigPicture.height + "px";
+
+            this.smallOverlay.current.style.width = overlayProportions.visible.smallPicture.width + "px";
+            this.smallOverlay.current.style.height = overlayProportions.visible.smallPicture.width + "px";
+
+            this.smallOverlay.current.style.left = (overlayProportions.visible.offset.width - 3) + "px";
+            this.smallOverlay.current.style.top = (overlayProportions.visible.offset.height- 3) + "px";
+
+            // TODO Verify that image is big enough of overlays
+
+            this.setState({
+                calculationRun: true,
+                imageProportions: imageProportions,
+                overlayProportions: overlayProportions,
+            });
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateCalculation);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateCalculation);
     }
 
     render() {
         return (
             <div className='imageManager'>
-                <div className="imageManager_imageContainer">
+                <div className="imageManager_imageContainer imageContainer">
+                    <div
+                        className={"imageContainer_bigOverlay " + (this.state.calculationRun ? "" : "hidden")}
+                        ref={this.bigOverlay}
+                    >
+                    </div>
+                    <div
+                        className={"imageContainer_smallOverlay " + (this.state.calculationRun ? "" : "hidden")}
+                        ref={this.smallOverlay}
+                    >
+                    </div>
+
                     <img
-                        className="imageManager_image"
+                        className={"imageContainer_loading " + (this.state.imageLoaded ? "hidden" : "")}
+                        src="build/loading.GIF"
+                        alt="loading"
+                    />
+
+                    <ImageOnLoad
+                        ref={this.image}
+                        className={"imageManager_image " + (this.state.imageLoaded ? "" : "hidden")}
                         key={this.props.image.preview}
                         src={this.props.image.preview}
+                        onLoad={this.onImageLoad}
                         alt="Image"
                     />
                 </div>
@@ -39,6 +145,15 @@ class ImageManager extends Component {
                         onClick={this.onResetClick}
                     >
                         Reset
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className="imageManager_apply"
+                        onClick={this.onApplyClick}
+                        disabled={this.state.invalid}
+                    >
+                        Apply
                     </Button>
                 </div>
             </div>
---
+++ src/helpers/imageManagerHelper.jsx
@@ -0,0 +1,126 @@
+class imageManagerHelper {
+    calculateProportions(imageProportions, overlayProportions, proportions) {
+        let ratio = proportions.big.bigPicture.height / proportions.big.bigPicture.width;
+
+        let bigOverlayWidth = imageProportions.width;
+        let bigOverlayHeight = bigOverlayWidth * ratio;
+        let naturalBigOverlayWidth = imageProportions.naturalWidth;
+        let naturalBigOverlayHeight = naturalBigOverlayWidth * ratio;
+
+        overlayProportions.visible.bigPicture.width = bigOverlayWidth;
+        overlayProportions.visible.bigPicture.height = bigOverlayHeight;
+        overlayProportions.natural.bigPicture.width = naturalBigOverlayWidth;
+        overlayProportions.natural.bigPicture.height = naturalBigOverlayHeight;
+
+        ratio = bigOverlayWidth / proportions.big.bigPicture.width;
+        let naturalRatio = naturalBigOverlayWidth / proportions.big.bigPicture.width;
+
+        let smallOverlaySize = proportions.big.smallPicture.width * ratio;
+        let naturalSmallOverlaySize = proportions.big.smallPicture.width * naturalRatio;
+
+        overlayProportions.visible.smallPicture.height =
+            overlayProportions.visible.smallPicture.width = smallOverlaySize;
+        overlayProportions.natural.smallPicture.height =
+            overlayProportions.natural.smallPicture.width = naturalSmallOverlaySize;
+
+        let offsetWidth = proportions.big.offset.width * ratio;
+        let offsetHeight = bigOverlayHeight - (proportions.big.offset.height * ratio);
+        let naturalOffsetWidth = proportions.big.offset.width * naturalRatio;
+        let naturalOffsetHeight = naturalBigOverlayHeight - (proportions.big.offset.height * naturalRatio);
+
+
+        overlayProportions.visible.offset.width = offsetWidth;
+        overlayProportions.visible.offset.height = offsetHeight;
+        overlayProportions.natural.offset.width = naturalOffsetWidth;
+        overlayProportions.natural.offset.height = naturalOffsetHeight;
+
+        return overlayProportions;
+    }
+
+    defaultValues() {
+        return {
+            imageProportions: {
+                "width": 0,
+                "height": 0,
+                "naturalWidth": 0,
+                "naturalHeight": 0,
+            },
+            overlayProportions: {
+                "natural": {
+                    "bigPicture": {
+                        "width": 0,
+                        "height": 0,
+                    },
+                    "smallPicture": {
+                        "width": 0,
+                        "height": 0,
+                    },
+                    "offset": {
+                        "width": 0,
+                        "height": 0,
+                    },
+                },
+                "visible": {
+                    "bigPicture": {
+                        "width": 0,
+                        "height": 0,
+                    },
+                    "smallPicture": {
+                        "width": 0,
+                        "height": 0,
+                    },
+                    "offset": {
+                        "width": 0,
+                        "height": 0,
+                    },
+                },
+            },
+            proportions: {
+                "small": {
+                    "bigPicture": {
+                        "width": 850,
+                        "height": 315,
+                    },
+                    "smallPicture": {
+                        "width": 160,
+                        "height": 160,
+                    },
+                    "offset": {
+                        "width": 20,
+                        "height": 138,
+                    },
+                },
+                "medium": {
+                    "bigPicture": {
+                        "width": 1024,
+                        "height": 380,
+                    },
+                    "smallPicture": {
+                        "width": 193,
+                        "height": 193,
+                    },
+                    "offset": {
+                        "width": 24,
+                        "height": 167,
+                    },
+                },
+                "big": {
+                    "bigPicture": {
+                        "width": 2048,
+                        "height": 759,
+                    },
+                    "smallPicture": {
+                        "width": 386,
+                        "height": 386,
+                    },
+                    "offset": {
+                        "width": 48,
+                        "height": 333,
+                    },
+                },
+            }
+        };
+    }
+}
+
+export default new imageManagerHelper();
